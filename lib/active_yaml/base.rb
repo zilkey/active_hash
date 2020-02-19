ed509bb8407f32059b3150f94e570fea3ccb0c60
Reed Law <reed@doctible.com>
2020-02-19 15:12:26 -0800

    add option to disable erb parsing

--- lib/active_yaml/base.rb
+++ lib/active_yaml/base.rb
@@ -4,7 +4,14 @@ module ActiveYaml
 
   class Base < ActiveFile::Base
     extend ActiveFile::HashAndArrayFiles
+
+    class_attribute :erb_disabled
+
     class << self
+      def set_erb_disabled(erb_disabled)
+        self.erb_disabled = erb_disabled
+      end
+
       def load_file
         if (data = raw_data).is_a?(Array)
           data
@@ -19,7 +26,9 @@ module ActiveYaml
 
       private
       def load_path(path)
-        YAML.load(ERB.new(File.read(path)).result)
+        file = File.read(path)
+        result = erb_disabled ? file : ERB.new(file).result
+        YAML.load(result)
       end
     end
   end
